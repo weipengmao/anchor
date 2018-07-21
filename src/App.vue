61e46209e919d2939f142d177f6632a49a27ce95
weipengmao <412948637@qq.com>
2018-07-21 10:56:01 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth*20/375 + 'px';
+window.onresize = function() {
+    console.log(window.innerWidth);
+    document.getElementsByTagName('html')[0].style.fontSize = window.innerWidth*20/375 + 'px';
+}
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
